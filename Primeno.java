a9a3a9d03a93ab4068e80ef8de7d87c9e98c831b
Sridhar <107329849+Ceedhu@users.noreply.github.com>
2022-07-10 08:59:58 +0530

    Add files via upload

---
+++ Primeno.java
@@ -0,0 +1,21 @@
+package week1.day01.assignments;
+
+public class Primeno {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=10,i;
+		boolean prime=true;
+		for(i=2;i<n;i++)
+		{
+			if(n%i==0)
+			{
+				prime=false;
+				break;
+			}
+		}
+			System.out.println(prime);
+		
+	}
+
+}
